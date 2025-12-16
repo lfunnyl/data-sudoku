97eb1ed20fc8f6333a59ed670e72d664cb92df8b
İbrahim Aydoğdu <ibrahimsitkiaydogdu@gmail.com>
2025-12-16 19:15:12 +0300

    sodokuyu çözen algoritmayi yazdım

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,33 @@
-# pylint: disable=missing-docstring
-
-
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    # Eşittir (=) işaretini koyduğumuzdan emin olalım:
+    valid_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    # 1. ADIM: SATIRLARI (ROWS) KONTROL ET
+    for row in grid:
+        if set(row) != valid_set:
+            return False
+
+    # 2. ADIM: SÜTUNLARI (COLUMNS) KONTROL ET
+    for j in range(9):
+        column = []
+        for i in range(9):
+            column.append(grid[i][j])
+        
+        if set(column) != valid_set:
+            return False
+
+    # 3. ADIM: 3x3 KARELERİ KONTROL ET
+    # Tabloyu 3'er 3'er atlayarak geziyoruz (0, 3, 6)
+    # i ve j, her bir 3x3'lük bloğun sol üst köşesidir.
+    for i in range(0, 9, 3): 
+        for j in range(0, 9, 3):
+            square = []
+            # Şimdi o bloğun içindeki 3x3 alanı tarıyoruz
+            for x in range(3):
+                for y in range(3):
+                    square.append(grid[i+x][j+y])
+            
+            if set(square) != valid_set:
+                return False
+
+    return True
